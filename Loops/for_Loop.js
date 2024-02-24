56a1d3d5474e8fd99176ad726c05fd738b105020
Pratik Bagale <pratikbagale8@gmail.com>
2024-02-24 14:57:44 +0530

    Add for_Loop.js

---
+++ Loops/for_Loop.js
@@ -0,0 +1,12 @@
+// // Loops are used to execute a piece of code again & again
+
+// // syntax  for ( three statements ) {
+// //         console.log("       ");
+// //   }
+
+for (let i = 1; i <= 5; i++) {
+    console.log("apna collage");
+}
+
+console.log("loop has ended");
+
