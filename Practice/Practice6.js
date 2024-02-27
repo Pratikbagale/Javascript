1f3d5e42e8e71d549b914409c73034aef929a4c4
Pratik Bagale <pratikbagale8@gmail.com>
2024-02-27 13:59:18 +0530

    Add changes in the Javascript directory

---
+++ Practice/Practice6.js
@@ -0,0 +1,9 @@
+// String is a sequence of character used to represent text..
+
+let obj = {
+    item: "Pen",
+    price: 25,
+};
+
+let output = `the cost os ${obj.item} is ${obj.price} rupees`;
+console.log(output);
