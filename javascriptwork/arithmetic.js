851efba91b8cb32f1efc26e95bc40dc1595e3ba5
Pratik Bagale <pratikbagale8@gmail.com>
2024-02-27 16:19:15 +0530

    Add arithmetic.js

---
+++ javascriptwork/arithmetic.js
@@ -0,0 +1,10 @@
+//Arithmetic operator
+let a = 5;
+let b = 2;
+console.log("a= ", a, " & b = ", b);
+console.log("a+b= ", a + b);
+console.log("a-b= ", a - b);
+console.log("a*b= ", a * b);
+console.log("a/b= ", a / b);
+console.log("a**b= ", a ** b);//a=5 and b=2 then 2 is a power of 5..5*5=25
+
