d56bfcfc100070aa54b765e76086f232a1ad4d99
Pratik Bagale <pratikbagale8@gmail.com>
2024-03-01 18:15:53 +0530

    Add code

---
+++ javascriptwork/unaryoperator.js
@@ -0,0 +1,25 @@
+// //unary operator
+// let a = 5;
+// let b = 2;
+// console.log("a = ", a, "& b = ", b);
+// a++;// post increment 
+// b--;// post decrement
+// ++a;//pre increment 
+// --b;//pre decrement 
+// console.log("++a= ", a);//6
+// console.log("--b= ", b);
+// console.log("a++= ", a);
+// console.log("b++= ", b);
+
+let a = 5;
+let b = 2;
+
+console.log("a= ", a, "&b= ", b);
+
+//console.log("a++= ", a++);
+//console.log("a =  ", a);
+
+console.log("--a = ", --a);
+console.log("a-- = ", a--);
+console.log("a=  ", a);
+
