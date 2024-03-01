0b473e4198b906c349ab551f7ca72f5f7da5f51f
Pratik Bagale <pratikbagale8@gmail.com>
2024-03-01 18:17:04 +0530

    Add code

---
+++ javascriptwork/assignmentoperator.js
@@ -0,0 +1,16 @@
+// Assignment operator
+
+let a = 5;
+let b = 2;
+let c = 40;
+let d = 50;
+
+a += 4;// a=a+4
+b -= 4; //b=b-4
+c /= 2;
+d %= 2;
+
+console.log("a = ", a);
+console.log(" b =  ", b);
+console.log(" c=  ", c);
+console.log("d= ", d);
