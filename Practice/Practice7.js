e911ba4fe185bca5ded4b404b753f5afce544aa8
Pratik Bagale <pratikbagale8@gmail.com>
2024-03-02 14:20:59 +0530

    Add code

---
+++ Practice/Practice7.js
@@ -0,0 +1,10 @@
+//  template Literals
+
+let obj = {
+    item: "Pen",
+    price: 10,
+};
+console.log("the cost of", obj.item, " is", obj.price);
+
+let specialString = `this is a template literal`;
+console.log(typeof specialString);
