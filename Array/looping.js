66722f1ca0ab97c14e9a292ed30163836d609d95
Pratik Bagale <pratikbagale8@gmail.com>
2024-03-16 16:59:18 +0530

    Add code

---
+++ Array/looping.js
@@ -0,0 +1,7 @@
+let heroes = ["ironman", "thor", "hulk", "shaktiman", "antman", "spiderman"];
+
+//for loop
+for (let i = 0; i < heroes; i++) {
+    console.log(heroes[i]);
+}
+//console.log("heroes: ", heroes);
