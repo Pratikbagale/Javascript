0809d4fb3e90b612e00a2d64f6fff0280ee9e6e9
Pratik Bagale <pratikbagale8@gmail.com>
2024-03-16 16:58:31 +0530

    Add code

---
+++ Array/array.js
@@ -0,0 +1,9 @@
+let marks = [97, 82, 75, 64, 34];
+console.log("marks: ", marks);
+
+// console.log(marks.length);// property
+
+// let Heroes = ["ironman", "thor", "spiderman", "shaktiman", "hulk"];
+// console.log("Heroes: ", Heroes);
+// console.log(Heroes.length);//property
+
