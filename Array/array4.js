dcdc2cee2ea22b2cfb2a103bc3aa0f66ff0bf6d4
Pratik Bagale <pratikbagale8@gmail.com>
2024-04-03 18:06:24 +0530

    Add code

---
+++ Array/array4.js
@@ -0,0 +1,9 @@
+// Concat():joins multiple arrays & returns result
+
+let bollywood_heroes = ["akshaykumar", "tigershroof", "sunilshetty", "salmankhan"];
+
+let tollywood_heroes = ["vijaydevorkonda", "maheshbabu", "alluarjun"];
+
+let heroes = bollywood_heroes.concat(tollywood_heroes);
+
+console.log(heroes);
