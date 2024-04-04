18a6284d12382e4e2abadd8d175414513dd48a39
Pratik Bagale <pratikbagale8@gmail.com>
2024-04-04 19:01:18 +0530

    Add code

---
+++ Array/push.js
@@ -0,0 +1,28 @@
+//  Push() Method
+
+let menue = ["potato", "tomato", "ginger"];
+
+menue.push("chips", "burger", "pavvada", "paneer");
+
+console.log(menue);
+
+// Pop
+
+
+//menue.pop();
+
+console.log(menue);
+
+let deletedItem = menue.pop();
+
+console.log(menue);
+
+console.log("deleted", deletedItem);
+
+
+//  toString
+
+let foodItems = ["potato", "apple", "litchi", "tomato"];
+console.log(foodItems);
+console.log(foodItems.toString());
+
