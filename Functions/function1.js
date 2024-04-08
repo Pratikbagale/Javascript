0f3b1c763e8c73f32786f6690695f5e96cc4b86d
Pratik Bagale <pratikbagale8@gmail.com>
2024-04-08 16:05:43 +0530

    Add code

---
+++ Functions/function1.js
@@ -0,0 +1,12 @@
+// function myFunction() {
+//     console.log("Welcome to Javascript");
+//     console.log("we are learning Javascript: ");
+// }
+// myFunction();
+
+function myFunction(msg) {
+    // parameter -> input
+    console.log(msg );
+}
+
+myFunction("Javascript");// argument
