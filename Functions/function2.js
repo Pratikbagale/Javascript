0df28a32dc1630acd681a95a3eb1617d0d09a551
Pratik Bagale <pratikbagale8@gmail.com>
2024-04-08 16:06:04 +0530

    Add code

---
+++ Functions/function2.js
@@ -0,0 +1,13 @@
+//  Function --> 2numbers,sum
+
+// function sum(a, b)
+//  {
+//     console.log(a + b);
+// }
+
+function sum(x, y) {
+    s = x + y;
+    return s;
+}
+let val = sum(3, 4);
+console.log(val);
