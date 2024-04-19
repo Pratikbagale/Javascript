26317035f0f9aa28c689eb2bd08a0a2142a008cc
Pratik Bagale <pratikbagale8@gmail.com>
2024-04-19 12:57:27 +0530

    Add code

---
+++ Functions/arrowfunction.js
@@ -0,0 +1,11 @@
+// Sum Function
+// It is a part of modern javascript
+
+//const arrowSum = (a,b) => {
+//  console.log(a+b);
+//};
+
+// Multiplication function
+const arrowMul = (a, b) => {
+    console.log(a * b);
+};
