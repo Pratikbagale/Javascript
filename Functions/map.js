b62ded01eecbe9d1fa5b2d5f4f62b2e3178d67f5
Pratik Bagale <pratikbagale8@gmail.com>
2024-04-21 23:46:33 +0530

    Add code

---
+++ Functions/map.js
@@ -0,0 +1,9 @@
+// It creates a new array with results of some operation.
+
+let nums = [67, 52, 39];
+
+let newArr = nums.map((val) => {
+    return val**2;
+});
+
+console.log(newArr);
---
+++ javascriptwork/logicaloperator.js
@@ -0,0 +1,6 @@
+// Logical operator
+
+let a = 6;
+let b = 5;
+
+console.log(" cond1 && cond2 =  ", a < b && a === 6);
