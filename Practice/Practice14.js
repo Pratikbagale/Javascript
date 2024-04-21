bfa6f0eee5372feaa124f8b459e65057eb758808
Pratik Bagale <pratikbagale8@gmail.com>
2024-04-21 23:16:22 +0530

    Add code

---
+++ Practice/Practice14.js
@@ -0,0 +1,7 @@
+// Q.For a give array of numbers,print the square of each value using the forEach loop..
+
+let numbers = [2, 3, 4, 5, 6];
+
+numbers.forEach((numbers) => {
+    console.log(numbers * numbers);// num**2
+});
