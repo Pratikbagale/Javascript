48b5fd4a69f291a999558689797ba29efe4ba038
Pratik Bagale <pratikbagale8@gmail.com>
2024-04-24 19:18:44 +0530

    Add code

---
+++ DOM1/class.js
@@ -0,0 +1,14 @@
+
+// // DOM Manipulaton 
+// Selecting with class
+
+// let heading = document.getElementById("heading");//h1
+
+// console.dir(heading);
+
+let button = document.getElementById("myId");
+console.dir(button);
+
+let headings = document.getElementsByClassName("myClass");
+console.dir(headings);
+console.log(headings);
