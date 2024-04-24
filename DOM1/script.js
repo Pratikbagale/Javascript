1b890ef1e9a7724365537e210e4eef066141f74d
Pratik Bagale <pratikbagale8@gmail.com>
2024-04-24 19:18:02 +0530

    Add code

---
+++ DOM1/script.js
@@ -0,0 +1,10 @@
+// // console.log("Hello!");
+// // window.console.log("Hello2!");
+// // alert("Start");
+
+
+// console.dir(document.body.childNodes[1]);
+
+let heading = document.getElementById("heading");//h1
+
+console.dir(heading);
