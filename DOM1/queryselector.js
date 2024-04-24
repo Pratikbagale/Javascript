c44ba34c325b9bbaa8decad7c14b5e66c85a85b8
Pratik Bagale <pratikbagale8@gmail.com>
2024-04-24 19:19:20 +0530

    Add code

---
+++ DOM1/queryselector.js
@@ -0,0 +1,13 @@
+
+//  DOM Manipulaton 
+// Selecting with querySelector
+
+
+// let elements = document.querySelector("p");
+// console.dir(elements);
+
+let firstEl = document.querySelector("p"); // first element
+console.dir(firstEl);
+
+let allEl = document.querySelectorAll("p"); // all elements
+console.dir(allEl);
